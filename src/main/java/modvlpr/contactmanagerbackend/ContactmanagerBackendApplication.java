267da5220c88be37d71a8006302ce2693fc9aec9
mofsdev-private <modevprojects@protonmail.com>
2024-04-06 18:20:29 -0500

    Initial commit

---
+++ src/main/java/modvlpr/contactmanagerbackend/ContactmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package modvlpr.contactmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactmanagerBackendApplication.class, args);
+	}
+
+}
